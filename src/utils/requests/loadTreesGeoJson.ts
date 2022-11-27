d67a3f41a38935bae5af48f63ad5ba42730bcbda
Georg Hans <gehans@outlook.de>
2022-11-27 14:28:51 +0100

    local trees geoJSON to fix CORS

--- src/utils/requests/loadTreesGeoJson.ts
+++ src/utils/requests/loadTreesGeoJson.ts
@@ -2,7 +2,7 @@ import { dsv as d3Dsv, GeoGeometryObjects } from 'd3';
 import { ExtendedFeatureCollection, ExtendedFeature } from 'd3-geo';
 import { Tree, TreeGeojsonFeatureProperties } from '../../common/interfaces';
 
-var isLocalTesting = process.env.LOCAL_TESTING
+var isLocalTesting = process.env.LOCAL_TESTING;
 
 function createGeojson(trees: Tree[]): ExtendedFeatureCollection {
   const geojson: ExtendedFeatureCollection = {
@@ -42,31 +42,37 @@ function createGeojson(trees: Tree[]): ExtendedFeatureCollection {
 }
 
 export const loadTreesGeoJson = async (): Promise<ExtendedFeatureCollection> => {
-  if (isLocalTesting) {
+  console.log('LOCAL TESTING', isLocalTesting);
+  console.log('data url', process.env.TREE_DATA_URL);
+  if (false) {
     // http://localhost:8080/tree/id-9135
-    const data = [{
-      'id': 'id-9135',
-      'lng': 12.346080918353433,
-      'lat': 51.328492865964243,
-      'radolan_sum': 820,
-      'age': 5
-    },{
-      'id': '4110',
-      'lng': 12.321279644966125,
-      'lat': 51.320883607664406,
-      'radolan_sum': 820,
-      'age': 0
-    },,{
-      'id': '3203',
-      'lng': 12.476344000378111,
-      'lat': 51.424873921079637,
-      'radolan_sum': 820,
-      'age': undefined
-    }]
+    const data = [
+      {
+        id: 'id-9135',
+        lng: 12.346080918353433,
+        lat: 51.328492865964243,
+        radolan_sum: 820,
+        age: 5,
+      },
+      {
+        id: '4110',
+        lng: 12.321279644966125,
+        lat: 51.320883607664406,
+        radolan_sum: 820,
+        age: 0,
+      },
+      {
+        id: '3203',
+        lng: 12.476344000378111,
+        lat: 51.424873921079637,
+        radolan_sum: 820,
+        age: undefined,
+      },
+    ];
     const geojson = createGeojson((data as unknown) as Tree[]);
     return geojson;
   } else {
-    const dataUrl = process.env.TREE_DATA_URL || 'https://trees-radolan-harvester-leipzig-dev.s3.eu-central-1.amazonaws.com/trees.csv.gz';
+    const dataUrl = process.env.TREE_DATA_URL || '/data/trees.csv.gz';
     const data = await d3Dsv(',', dataUrl, { cache: 'force-cache' });
     const geojson = createGeojson((data as unknown) as Tree[]);
     return geojson;
